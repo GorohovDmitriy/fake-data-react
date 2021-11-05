a2421c975528b5c099ae550d531038e0476ac6c1
dmitriy2893 <dmitriy2893@gmail.com>
2021-11-05 14:55:35 +0300

    ADD: commin end

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router('./db.json');
-const middlewares = defaults({
-	static: './build'
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+	static: './build',
 });
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
 
+const PORT = process.env.PORT || 8000;
+
+server.use(middlewares);
 server.use(router);
 server.listen(PORT, () => {
 	console.log('Server is running');
